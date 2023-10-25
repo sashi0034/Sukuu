5bd62ad1fb6516f7ed26edc07a40f10df9cc0b24
sashi0034 <satoshi3104.nishi@gmail.com>
2023-10-25 20:37:39 +0900

    アイテムや階段なども表示されるように実装

--- Sukuu/Play/UI/UiMiniMap.cpp
+++ Sukuu/Play/UI/UiMiniMap.cpp
@@ -24,12 +24,39 @@ struct Play::UiMiniMap::Impl
 {
 	Grid<ArriveFlag> m_flag{};
 	RenderTexture m_renderTexture{};
+	Array<Point> m_discoveredItemPoints{};
 
 	void Update()
 	{
 		checkRefreshMap();
-		(void)m_renderTexture
-			.draw(GetTomlParameter<Vec2>(U"play.ui_minimap.screen_point"));
+		Transformer2D transform{Mat3x2::Translate(GetTomlParameter<Vec2>(U"play.ui_minimap.screen_point"))};
+
+		// マップ描画
+		(void)m_renderTexture.draw();
+
+		const int rc = getRenderCellSize();
+
+		auto&& scene = PlayScene::Instance();
+
+		if (static_cast<int>(Scene::Time() * 10) % 5 >= 2)
+		{
+			// プレイヤー描画
+			const auto playerPos =
+				scene.GetPlayer().CurrentPos().actualPos.movedBy(Point{CellPx_24, CellPx_24} / 2) / CellPx_24;
+			(void)Circle(playerPos * rc, rc / 2).draw(GetTomlParameter<Color>(U"play.ui_minimap.player_color"));
+		}
+
+		const auto itemColor = GetTomlParameter<Color>(U"play.ui_minimap.item_color");
+		for (int i = m_discoveredItemPoints.size() - 1; i >= 0; i--)
+		{
+			// アイテム描画
+			auto&& p = m_discoveredItemPoints[i];
+			if (scene.GetGimmick()[p] == GimmickKind::None)
+			{
+				m_discoveredItemPoints.remove_at(i);
+			}
+			(void)Circle((p * rc).movedBy(rc / 2, rc / 2), rc / 2).draw(itemColor);
+		}
 	}
 
 private:
@@ -53,6 +80,7 @@ private:
 			}
 		};
 
+		// 新たな範囲を調査
 		for (const auto p : step(Size{1, 1} * (vision + 1) * 2))
 		{
 			auto checking = playerPoint + p.movedBy(-vision, -vision);
@@ -63,24 +91,43 @@ private:
 
 			if (CanMovePointAt(map, checking))
 			{
-				// 描画
-				const auto drawingTl = checking * renderCell;
-				const int rc1 = renderCell; // - 1;
-				Rect(drawingTl, Size{1, 1} * renderCell)
-					.draw(GetTomlParameter<Color>(U"play.ui_minimap.movable_color"));
-
-				// 境界を描画
-				const auto borderColor = GetTomlParameter<Color>(U"play.ui_minimap.border_color");
-				if (CanMovePointTo(map, checking, Dir4::Right) == false)
-					(void)Line(drawingTl.movedBy(rc1, 0), drawingTl.movedBy(rc1, rc1)).draw(borderColor);
-				if (CanMovePointTo(map, checking, Dir4::Up) == false)
-					(void)Line(drawingTl, drawingTl.movedBy(rc1, 0)).draw(borderColor);
-				if (CanMovePointTo(map, checking, Dir4::Left) == false)
-					(void)Line(drawingTl, drawingTl.movedBy(0, rc1)).draw(borderColor);
-				if (CanMovePointTo(map, checking, Dir4::Down) == false)
-					(void)Line(drawingTl.movedBy(0, rc1), drawingTl.movedBy(rc1, rc1)).draw(borderColor);
+				drawCellAt(map, renderCell, checking);
 			}
 		}
+	}
+
+	void drawCellAt(const MapGrid& map, int renderCell, const Point& checking)
+	{
+		auto&& gimmick = PlayScene::Instance().GetGimmick();
+
+		// 描画
+		const auto drawingTl = checking * renderCell;
+		const int rc1 = renderCell; // - 1;
+		Rect(drawingTl, Size{1, 1} * renderCell)
+			.draw(GetTomlParameter<Color>(U"play.ui_minimap.movable_color"));
+
+		if (gimmick[checking] == GimmickKind::Stairs)
+		{
+			// 階段を描画
+			Rect(drawingTl, (Size{1, 1} * renderCell).movedBy(-2, -2))
+				.drawFrame(2, GetTomlParameter<Color>(U"play.ui_minimap.stairs_color"));
+		}
+		if (GimmickToItem(gimmick[checking]) != ConsumableItem::None)
+		{
+			// アイテム発見
+			m_discoveredItemPoints.push_back(checking);
+		}
+
+		// 境界を描画
+		const auto borderColor = GetTomlParameter<Color>(U"play.ui_minimap.border_color");
+		if (CanMovePointTo(map, checking, Dir4::Right) == false)
+			(void)Line(drawingTl.movedBy(rc1, 0), drawingTl.movedBy(rc1, rc1)).draw(borderColor);
+		if (CanMovePointTo(map, checking, Dir4::Up) == false)
+			(void)Line(drawingTl, drawingTl.movedBy(rc1, 0)).draw(borderColor);
+		if (CanMovePointTo(map, checking, Dir4::Left) == false)
+			(void)Line(drawingTl, drawingTl.movedBy(0, rc1)).draw(borderColor);
+		if (CanMovePointTo(map, checking, Dir4::Down) == false)
+			(void)Line(drawingTl.movedBy(0, rc1), drawingTl.movedBy(rc1, rc1)).draw(borderColor);
 	}
 };
 
