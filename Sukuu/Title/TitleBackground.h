20f6277c3000a4a87c0d5166b3e54688c5e9ca46
sashi0034 <satoshi3104.nishi@gmail.com>
2023-10-26 16:30:54 +0900

    ファイル分離

---
+++ Sukuu/Title/TitleBackground.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "Forward.h"
+
+namespace Title
+{
+	class TitleBackground : public ActorBase
+	{
+	public:
+		TitleBackground();
+		void Init();
+		void Update() override;
+
+	private:
+		struct Impl;
+		std::shared_ptr<Impl> p_impl;
+	};
+}
--- Sukuu/Title/TitleScene.cpp
+++ Sukuu/Title/TitleScene.cpp
@@ -1,8 +1,9 @@
 ﻿#include "stdafx.h"
 #include "TitleScene.h"
 
-#include "Play/Forward.h"
-#include "Play/Map/BgMapDrawer.h"
+#include "TitleBackground.h"
+#include "Util/ActorContainer.h"
+
 
 namespace
 {
@@ -11,117 +12,21 @@ namespace
 	{
 		return Util::GetTomlParameter<T>(U"title.scene." + key);
 	}
-
-	constexpr Size maxMapSize{64, 64};
-	constexpr std::array<StringView, 16> mapData = {
-		// c:    7       f
-		U"                ", // 00
-		U"                ", // 01
-		U"                ", // 02
-		U"       --       ", // 03
-		U"       --       ", // 04
-		U"     ------     ", // 05
-		U"     ------     ", // 06
-		U"  ------------  ", // 07
-		U"  ------------  ", // 08
-		U"     ------     ", // 09
-		U"     ------     ", // 0a
-		U"       --       ", // 0b
-		U"       --       ", // 0c
-		U"       --       ", // 0d
-		U"                ", // 0e
-		U"                ", // 0f
-	};
 }
 
 struct Title::TitleScene::Impl
 {
 	bool m_concluded{};
-	RenderTexture m_mapTexture{};
-	const MSRenderTexture m_renderTexture{Scene::Size(), TextureFormat::R8G8B8A8_Unorm_SRGB, HasDepth::Yes};
-	SimpleFollowCamera3D m_camera{};
-	double m_cameraFollowDeg{};
+	TitleBackground m_bg{};
 
-	void Init()
+	void Init(ActorBase& self)
 	{
-		constexpr int mapW = mapData[0].size();
-		constexpr int mapH = mapData.size();
-		constexpr auto mapSize = Size(mapW, mapH);
-
-		m_mapTexture = RenderTexture(maxMapSize * Play::CellPx_24);
-		Play::MapGrid mapGrid{maxMapSize};
-		for (const auto p : step(maxMapSize))
-			mapGrid.At(p).kind = Play::TerrainKind::Wall;
-		for (const auto p : step(mapSize))
-		{
-			auto targetP = p.movedBy((maxMapSize - mapSize) / 2);
-			switch (mapData[p.y][p.x])
-			{
-			case U'-':
-				mapGrid.At(targetP).kind = Play::TerrainKind::Floor;
-				break;
-			default:
-				mapGrid.At(targetP).kind = Play::TerrainKind::Wall;
-				break;
-			}
-		}
-
-		ScopedRenderTarget2D target{m_mapTexture};
-		for (const auto p : step(maxMapSize))
-		{
-			Play::DrawBgMapTileAt(mapGrid, p.x, p.y);
-		}
-
-		resetCamera();
+		m_bg = self.AsParent().Birth(TitleBackground());
+		m_bg.Init();
 	}
 
 	void Update()
 	{
-#if _DEBUG
-		if (KeyC.down())
-		{
-			resetCamera();
-		}
-#endif
-
-		m_cameraFollowDeg += GetDeltaTime() * getToml<double>(U"camera_rotation_speed");
-		m_camera.setTarget(getToml<Vec3>(U"camera_target_position"), ToRadians(m_cameraFollowDeg));
-		m_camera.setFollowOffset(getToml<double>(U"camera_follow_distance"), getToml<double>(U"camera_follow_height"));
-		m_camera.update(2.0, GetDeltaTime());
-
-		// 3D描画
-		draw3D();
-
-		// RenderTextureを2Dシーンに描画
-		apply3D();
-	}
-
-private:
-	void resetCamera()
-	{
-		m_camera = SimpleFollowCamera3D(m_renderTexture.size(),
-		                                ToRadians(getToml<double>(U"camera_fov")),
-		                                getToml<Vec3>(U"camera_initial_position"),
-		                                0.0_deg,
-		                                16.0,
-		                                8.0);
-	}
-
-	void draw3D() const
-	{
-		Graphics3D::SetCameraTransform(m_camera);
-
-		const ScopedRenderTarget3D target{m_renderTexture.clear(getToml<ColorF>(U"bg_color"))};
-		const ScopedRenderStates3D state{SamplerState::BorderNearest};
-
-		(void)Plane{maxMapSize.x * 2}.draw(m_mapTexture);
-	}
-
-	void apply3D() const
-	{
-		Graphics3D::Flush();
-		m_renderTexture.resolve();
-		Shader::LinearToScreen(m_renderTexture);
 	}
 };
 
@@ -134,7 +39,7 @@ namespace Title
 
 	void TitleScene::Init()
 	{
-		p_impl->Init();
+		p_impl->Init(*this);
 	}
 
 	void TitleScene::Update()
