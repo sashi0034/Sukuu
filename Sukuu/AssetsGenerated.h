e8b8b2de5e5fc875d0bda16db15e477a92264030
sashi0034 <satoshi3104.nishi@gmail.com>
2023-10-26 20:37:09 +0900

    画像ファイル追加

--- Sukuu/AssetsGenerated.h
+++ Sukuu/AssetsGenerated.h
@@ -6,19 +6,25 @@ namespace AssetImages
 	constexpr StringView bookmark_16x16 = U"asset/image/bookmark_16x16.png";
 	constexpr StringView brick_stylish_24x24 = U"asset/image/brick_stylish_24x24.png";
 	constexpr StringView brick_stylish_light_24x24 = U"asset/image/brick_stylish_light_24x24 .png";
+	constexpr StringView catfish_24x24 = U"asset/image/catfish_24x24.png";
 	constexpr StringView clock_16x16 = U"asset/image/clock_16x16.png";
+	constexpr StringView dango_wolf_24x24 = U"asset/image/dango_wolf_24x24.png";
 	constexpr StringView dark_tree_16x16 = U"asset/image/dark_tree_16x16.png";
 	constexpr StringView dark_tree_48x48 = U"asset/image/dark_tree_48x48.png";
 	constexpr StringView helmet_16x16 = U"asset/image/helmet_16x16.png";
 	constexpr StringView hourglass_16x16 = U"asset/image/hourglass_16x16.png";
 	constexpr StringView light_bulb_16x16 = U"asset/image/light_bulb_16x16.png";
+	constexpr StringView lion_32x32 = U"asset/image/lion_32x32.png";
 	constexpr StringView magma_tile_24x24 = U"asset/image/magma_tile_24x24.png";
 	constexpr StringView magnet_16x16 = U"asset/image/magnet_16x16.png";
 	constexpr StringView phine_32x32 = U"asset/image/phine_32x32.png";
 	constexpr StringView pin_16x16 = U"asset/image/pin_16x16.png";
 	constexpr StringView punicat_24x24 = U"asset/image/punicat_24x24.png";
+	constexpr StringView rust_ferris_24x24 = U"asset/image/rust_ferris_24x24.png";
+	constexpr StringView smile_mush_24x24 = U"asset/image/smile_mush_24x24.png";
 	constexpr StringView solt_16x16 = U"asset/image/solt_16x16.png";
 	constexpr StringView stairs_24x24 = U"asset/image/stairs_24x24.png";
+	constexpr StringView temple_knight_side_32x32 = U"asset/image/temple_knight_side_32x32.png";
 	constexpr StringView test_tube_16x16 = U"asset/image/test_tube_16x16.png";
 	constexpr StringView tex_aqua_noise = U"asset/image/tex_aqua_noise.png";
 	constexpr StringView tex_cosmos_noise = U"asset/image/tex_cosmos_noise.png";
@@ -33,19 +39,25 @@ namespace AssetImages
 			bookmark_16x16,
 			brick_stylish_24x24,
 			brick_stylish_light_24x24,
+			catfish_24x24,
 			clock_16x16,
+			dango_wolf_24x24,
 			dark_tree_16x16,
 			dark_tree_48x48,
 			helmet_16x16,
 			hourglass_16x16,
 			light_bulb_16x16,
+			lion_32x32,
 			magma_tile_24x24,
 			magnet_16x16,
 			phine_32x32,
 			pin_16x16,
 			punicat_24x24,
+			rust_ferris_24x24,
+			smile_mush_24x24,
 			solt_16x16,
 			stairs_24x24,
+			temple_knight_side_32x32,
 			test_tube_16x16,
 			tex_aqua_noise,
 			tex_cosmos_noise,
