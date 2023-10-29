1c091a8cf60ff5aa4a4fef6ebf65ab7e9caa5e87
sashi0034 <satoshi3104.nishi@gmail.com>
2023-10-29 11:50:14 +0900

    大体のエディタ機能を実装

--- MapEditor/Main.cpp
+++ MapEditor/Main.cpp
@@ -1,30 +1,134 @@
-﻿# include <Siv3D.hpp>
+﻿# include "stdafx.h"
+
+#include <iso646.h>
+
+namespace
+{
+	constexpr Size mapSize{65, 65};
+
+	void changeCharBy(Grid<char32_t>& cells, Point p, bool mirror, char32_t c)
+	{
+		if (cells.inBounds(p) == false) return;
+		cells[p] = c;
+		if (mirror) changeCharBy(cells, {cells.size().x - 1 - p.x, p.y}, false, c);
+	}
+
+	void changeAt(Grid<char32_t>& cells, Point p, bool mirror)
+	{
+		if (KeySpace.pressed()) changeCharBy(cells, p, mirror, U' ');
+		if (KeyMinus.pressed()) changeCharBy(cells, p, mirror, U'-');
+		if (KeyA.pressed()) changeCharBy(cells, p, mirror, U'A');
+	}
+}
 
 void Main()
 {
-	Scene::SetBackground(Palette::White);
+	Scene::SetBackground(ColorF{0.1});
 
-	constexpr int32 cellSize = 40;
+	Window::SetStyle(WindowStyle::Sizable);
+	Scene::SetResizeMode(ResizeMode::Keep);
 
-	// シーンのサイズとセルの大きさから縦横のセルの個数を計算
-	Grid<int32> cells(Scene::Size() / cellSize);
+	constexpr int32 cellSize = 10;
+	const Font font(FontMethod::SDF, cellSize, Typeface::Bold);
+
+	Camera2D camera2D{};
+	Grid<char32_t> cellGrid(mapSize, U' ');
+
+	size_t writeScaleIndex = 0;
+	bool mirrored{};
+	char32_t lastChar = U' ';
+	bool dragging{};
+	Point dragStart{};
 
 	while (System::Update())
 	{
-		// カーソルを手の形に
-		Cursor::RequestStyle(CursorStyle::Hand);
-
-		for (auto p : step(cells.size()))
+		[&]
 		{
-			const Rect rect{ (p * cellSize), cellSize };
-
-			if (rect.leftClicked())
+			// 内容更新
+			camera2D.update();
+			const Transformer2D t = camera2D.createTransformer();
+			for (auto p : step(cellGrid.size()))
 			{
-				// 0 → 1 → 2 →　3 → 0 → 1 → ... と遷移させる
-				++cells[p] %= 4;
-			}
+				const Rect rect{(p * cellSize), cellSize};
+				(void)rect.stretched(-1).draw(ColorF{0.2});
+				(void)font(cellGrid[p]).drawAt(rect.center(), Palette::White);
 
-			rect.stretched(-1).draw(ColorF{ 0.95 - cells[p] * 0.3 });
+				if (rect.intersects(Cursor::Pos()) == false) continue;
+
+				const auto before = cellGrid[p];
+				for (auto offset : step(Size{1, 1} * static_cast<int>(1 + writeScaleIndex * 2)))
+				{
+					// 普通に書き換え
+					changeAt(cellGrid, p.movedBy(offset - Size{1, 1} * static_cast<int>(writeScaleIndex)), mirrored);
+				}
+				if (before != cellGrid[p]) lastChar = cellGrid[p];
+
+				// 塗りつぶし処理
+				if (MouseL.down())
+				{
+					dragging = true;
+					dragStart = p;
+				}
+				else if (dragging && MouseL.pressed())
+				{
+					Rect(dragStart * cellSize, (p - dragStart + Point{1, 1}) * cellSize)
+						.draw(ColorF{Palette::Yellow, 0.3});
+				}
+				else if (dragging && not MouseL.pressed())
+				{
+					dragging = false;
+					// 塗りつぶし実行
+					const Point tl{std::min(dragStart.x, p.x), std::min(dragStart.y, p.y)};
+					const Point br{std::max(dragStart.x, p.x), std::max(dragStart.y, p.y)};
+					for (const auto offset : step(br - tl + Point{1, 1}))
+					{
+						changeCharBy(cellGrid, tl.movedBy(offset), mirrored, lastChar);
+					}
+				}
+			}
+		}();
+
+		if (SimpleGUI::Button(U"Copy", Vec2{10, 10}))
+		{
+			String s{};
+			for (int y = 0; y < cellGrid.size().y; ++y)
+			{
+				s += U"U\"";
+				for (int x = 0; x < cellGrid.size().x; ++x)
+				{
+					s += cellGrid[y][x];
+				}
+				s += U"\",\n";
+			}
+			Clipboard::SetText(s);
 		}
+
+		if (SimpleGUI::Button(U"Paste", Vec2{110, 10}))
+		{
+			String s;
+			Clipboard::GetText(s);
+			int x{}, y{};
+			bool inner{};
+			for (const auto c : s)
+			{
+				if (c == U'\"')
+				{
+					if (inner)
+					{
+						x = 0;
+						y++;
+					}
+					inner = not inner;
+					continue;
+				}
+				if (not inner) continue;
+				if (cellGrid.inBounds({x, y})) cellGrid[{x, y}] = c;
+				x += 1;
+			}
+		}
+
+		SimpleGUI::CheckBox(mirrored, U"Mirror", {10, 50});
+
+		SimpleGUI::RadioButtons(writeScaleIndex, {U"Small", U"Medium", U"Large"}, {10, 90});
 	}
 }
