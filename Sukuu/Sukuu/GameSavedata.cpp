77a30a63873f0630f23cdbf255f7fe8790dd001c
sashi0034 <satoshi3104.nishi@gmail.com>
2023-11-02 01:05:18 +0900

    セーブデータ用実装

---
+++ Sukuu/Sukuu/GameSavedata.cpp
@@ -0,0 +1,26 @@
+﻿#include "stdafx.h"
+#include "GameSavedata.h"
+
+namespace
+{
+	constexpr String dataPath = U"savedata.dat";
+}
+
+namespace Sukuu
+{
+	Optional<GameSavedata> LoadSavedata()
+	{
+		BinaryReader reader{dataPath};
+		if (not reader) return none;
+		GameSavedata d;
+		reader.read(d);
+		return d;
+	}
+
+	void SaveSaveData(const GameSavedata& data)
+	{
+		BinaryWriter writer{dataPath};
+		if (not writer) return;
+		writer.write(data);
+	}
+}
---
+++ Sukuu/Sukuu/GameSavedata.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+namespace Sukuu
+{
+	struct GameSavedata
+	{
+		int bestReached{};
+		double completedTime{};
+	};
+
+	Optional<GameSavedata> LoadSavedata();
+	void SaveSavedata(const GameSavedata& data);
+}
