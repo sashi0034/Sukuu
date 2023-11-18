a882c13025001953afefb3084e54a089cb1e274d
sashi0034 <satoshi3104.nishi@gmail.com>
2023-11-18 18:00:56 +0900

    クリックでポーズ戻ってなかったのを修正

--- Sukuu/Play/Other/PlayingPause.cpp
+++ Sukuu/Play/Other/PlayingPause.cpp
@@ -10,6 +10,8 @@ namespace Play
 	{
 		if (not m_pauseAllowed) return;
 
+		const Transformer2D transform{Mat3x2::Identity(), TransformCursor::Yes, Transformer2D::Target::SetLocal};
+
 		if (not Window::GetState().focused) m_paused = true;
 		else if (KeyEscape.down()) m_paused = not m_paused;
 		else if (m_paused && Util::IsSceneLeftClicked()) m_paused = false;
@@ -17,7 +19,6 @@ namespace Play
 		if (not m_paused) return;
 
 		const ScopedRenderStates2D sampler{SamplerState::ClampLinear};
-		const Transformer2D transform{Mat3x2::Identity(), Transformer2D::Target::SetLocal};
 
 		Rect(Scene::Size()).draw(ColorF{0.2, 0.7});
 
