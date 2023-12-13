67cb50a90d3109289e937a3571c80a32782ad88b
sashi0034 <satoshi3104.nishi@gmail.com>
2023-12-13 15:14:44 +0900

    ボタン配置登録

--- Sukuu/Gm/GamepadRegistering.cpp
+++ Sukuu/Gm/GamepadRegistering.cpp
@@ -20,6 +20,7 @@ namespace
 
 	constexpr ColorF blackColor = ColorF(0.3);
 	constexpr ColorF grayColor = Palette::Gray;
+	constexpr ColorF redColor = Palette::Red;
 
 	int getFontSize()
 	{
@@ -37,12 +38,12 @@ namespace
 
 		const auto titleText = FontAsset(AssetKeys::RocknRoll_24_Bitmap)(U"コントローラー登録画面");
 		const auto titlePos = Vec2{1, 1} * getToml<int>(U"top");
-		(void)titleText.region(Arg::leftCenter = titlePos).stretched(8).draw(grayColor);
+		(void)titleText.region(Arg::leftCenter = titlePos).stretched(16, 8).draw(grayColor);
 		titleText.draw(Arg::leftCenter = titlePos, ColorF(1.0));
 
 		const auto bottom = Vec2{Scene::Center().x, Scene::Size().y};
 		const auto bottom1 = bottom.movedBy(0, -getToml<int>(U"bottom1"));
-		(void)FontAsset(AssetKeys::RocknRoll_Sdf)(U"Enter スキップ\tBackspace 一つ戻る\tEsc キャンセルして戻る").drawAt(
+		(void)FontAsset(AssetKeys::RocknRoll_Sdf)(U"Backspace 一つ戻る\tEsc キャンセルして戻る").drawAt(
 			fontSize, bottom1, blackColor);
 		FontAsset(AssetKeys::RocknRoll_Sdf)(U"設定したいボタンを押してください").drawAt(
 			fontSize, bottom.movedBy(0, -getToml<int>(U"bottom2")), blackColor);
@@ -64,6 +65,41 @@ namespace
 		     .draw(Arg::leftCenter = Vec2{getToml<int>(U"keyboard_left"), bottom1.y});
 	}
 
+	void drawButtonLiteral(bool isEnabled, const String& bp, const String& buttonLiteral)
+	{
+		(void)FontAsset(AssetKeys::RocknRoll_Sdf)(buttonLiteral)
+			.drawAt(getToml<int>(U"bs_text"), getToml<Point>(bp), isEnabled ? ColorF(1) : blackColor);
+	}
+
+	void drawCircleButton(bool isEnabled, const String& bp, const String& buttonLiteral)
+	{
+		if (isEnabled)
+		{
+			(void)Circle(getToml<Point>(bp), getToml<int>(U"bs_circle")).draw(redColor);
+		}
+		drawButtonLiteral(isEnabled, bp, buttonLiteral);
+	}
+
+	void drawSquareButton(bool isEnabled, const String& bp)
+	{
+		if (isEnabled)
+			(void)Rect(Arg::center = getToml<Point>(bp), getToml<int>(U"bs_square")).rounded(8).draw(redColor);
+	}
+
+	void drawHorizontalButton(bool isEnabled, const String& bp, const String& buttonLiteral)
+	{
+		if (isEnabled)
+			(void)Rect(Arg::center = getToml<Point>(bp), getToml<Point>(U"bs_horizontal")).rounded(16).draw(redColor);
+		drawButtonLiteral(isEnabled, bp, buttonLiteral);
+	}
+
+	void drawVerticalButton(bool isEnabled, const String& bp, const String& buttonLiteral)
+	{
+		if (isEnabled)
+			(void)Rect(Arg::center = getToml<Point>(bp), getToml<Point>(U"bs_vertical")).rounded(16).draw(redColor);
+		drawButtonLiteral(isEnabled, bp, buttonLiteral);
+	}
+
 	void updateGamepad()
 	{
 		const double scale = getToml<double>(U"gamepad_scale");
@@ -72,6 +108,22 @@ namespace
 			.translated(0, getToml<int>(U"gamepad_y") * scale)
 		};
 		TextureAsset(AssetKeys::gamepad).resized(1024).drawAt(Scene::Center());
+
+		drawCircleButton(true, U"bp_a", U"A");
+		drawCircleButton(true, U"bp_b", U"B");
+		drawCircleButton(true, U"bp_x", U"X");
+		drawCircleButton(true, U"bp_y", U"Y");
+
+		drawSquareButton(true, U"bp_du");
+		drawSquareButton(true, U"bp_dl");
+		drawSquareButton(true, U"bp_dd");
+		drawSquareButton(true, U"bp_dr");
+
+		drawHorizontalButton(true, U"bp_lb", U"LB");
+		drawHorizontalButton(true, U"bp_rb", U"RB");
+
+		drawVerticalButton(true, U"bp_lt", U"LT");
+		drawVerticalButton(true, U"bp_rt", U"RT");
 	}
 
 	void loopInternal()
