360a9092e0a8f213a148420191556f4f041c1ca1
sashi0034 <satoshi3104.nishi@gmail.com>
2023-12-13 12:53:59 +0900

    コントローラー登録画面見た目設定

--- Sukuu/Gm/GamepadRegistering.cpp
+++ Sukuu/Gm/GamepadRegistering.cpp
@@ -13,35 +13,80 @@ namespace
 		return Util::GetTomlParameter<T>(U"gm.gamepad_registering." + key);
 	}
 
-	struct GamepadRegisteringState
+	struct InternalState
 	{
+		Texture keyboardEmoji{U"⌨"_emoji};
 	};
 
-	void drawTexts()
+	constexpr ColorF blackColor = ColorF(0.3);
+	constexpr ColorF grayColor = Palette::Gray;
+
+	int getFontSize()
 	{
-		FontAsset(AssetKeys::RocknRoll_Sdf)(U"コントローラー設定")
-			.drawAt(getToml<int>(U"font_size"), Vec2{Scene::Center().x, getToml<int>(U"top_y")}, Palette::Gray);
+		return getToml<int>(U"font_size");
+	}
+
+	ColorF getBgColor()
+	{
+		return getToml<ColorF>(U"bg");
+	}
+
+	void drawTexts(const InternalState& state)
+	{
+		const auto fontSize = getFontSize();
+
+		const auto titleText = FontAsset(AssetKeys::RocknRoll_24_Bitmap)(U"コントローラー登録画面");
+		const auto titlePos = Vec2{1, 1} * getToml<int>(U"top");
+		(void)titleText.region(Arg::leftCenter = titlePos).stretched(8).draw(grayColor);
+		titleText.draw(Arg::leftCenter = titlePos, ColorF(1.0));
+
+		const auto bottom = Vec2{Scene::Center().x, Scene::Size().y};
+		const auto bottom1 = bottom.movedBy(0, -getToml<int>(U"bottom1"));
+		(void)FontAsset(AssetKeys::RocknRoll_Sdf)(U"Enter スキップ\tBackspace 一つ戻る\tEsc キャンセルして戻る").drawAt(
+			fontSize, bottom1, blackColor);
+		FontAsset(AssetKeys::RocknRoll_Sdf)(U"設定したいボタンを押してください").drawAt(
+			fontSize, bottom.movedBy(0, -getToml<int>(U"bottom2")), blackColor);
+		const auto buttDesc = FontAsset(AssetKeys::RocknRoll_Sdf)(U"B BUTTON : DASH");
+		const auto bottom3 = bottom.movedBy(0, -getToml<int>(U"bottom3"));
+		(void)buttDesc.regionAt(fontSize, bottom3)
+		              .stretched(getToml<Point>(U"desc_padding"))
+		              .rounded(10)
+		              .drawShadow(Vec2{3, 3}, 12, 1).draw(grayColor)
+		              .draw(grayColor);
+		(void)buttDesc.drawAt(fontSize, bottom3, Palette::White);
+
+		constexpr int lineX = 64;
+		const int lineY = bottom.y - getToml<int>(U"line_y");
+		const int lineThickness = getToml<int>(U"line_thickness");
+		(void)Line(lineX, lineY, Scene::Size().x - lineX, lineY).draw(lineThickness, grayColor);
+
+		state.keyboardEmoji.resized(getToml<int>(U"keyboard_size"))
+		     .draw(Arg::leftCenter = Vec2{getToml<int>(U"keyboard_left"), bottom1.y});
 	}
 
 	void updateGamepad()
 	{
+		const double scale = getToml<double>(U"gamepad_scale");
 		Transformer2D transformer2D{
-			Mat3x2::Scale(getToml<double>(U"gamepad_scale"), Scene::Center().movedBy(0, getToml<int>(U"gamepad_y")))
+			Mat3x2::Scale(scale, Scene::Center())
+			.translated(0, getToml<int>(U"gamepad_y") * scale)
 		};
 		TextureAsset(AssetKeys::gamepad).resized(1024).drawAt(Scene::Center());
 	}
 
 	void loopInternal()
 	{
+		InternalState state{};
+
 		while (System::Update())
 		{
 			Util::RefreshTomlParameters();
-			Scene::SetBackground(getToml<ColorF>(U"bg"));
+			Scene::SetBackground(getBgColor());
 
-			ClearPrint();
-			Print(Cursor::Pos());
+			// ClearPrint();
+			// Print(Cursor::Pos());
 
-			drawTexts();
+			drawTexts(state);
 
 			updateGamepad();
 		}
