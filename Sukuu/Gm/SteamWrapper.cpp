7042fe8d49de956307f9d4e6f1906d917ab6c60c
sashi0034 <satoshi3104.nishi@gmail.com>
2024-08-13 22:50:01 +0900

    Steam App Id 追加

--- Sukuu/Gm/SteamWrapper.cpp
+++ Sukuu/Gm/SteamWrapper.cpp
@@ -1,21 +1,36 @@
 ﻿#include "stdafx.h"
 #include "SteamWrapper.h"
 #include "steam_api.h"
+#include <isteamgameserverstats.h>
 
 namespace
 {
+	constexpr int steamAppId = 3147480;
+
 	struct SteamWrapperAddon : IAddon
 	{
 		bool init() override
 		{
-			if (SteamAPI_Init())
+			if (SteamAPI_RestartAppIfNecessary(steamAppId))
+			{
+				Console.writeln(U"SteamAPI_RestartAppIfNecessary() true");
+			}
+			else
+			{
+				Console.writeln(U"SteamAPI_RestartAppIfNecessary() false");
+			}
+
+			SteamErrMsg err{};
+			const auto initRes = SteamAPI_InitEx(&err);
+			if (initRes == k_ESteamAPIInitResult_OK)
 			{
 				// FIXME: This is a temporary implementation.
 				Console.writeln(U"SteamAPI_Init() succeeded.");
 			}
 			else
 			{
-				Console.writeln(U"SteamAPI_Init() failed.");
+				Console.writeln(U"SteamAPI_Init() failed. {}"_fmt(static_cast<int>(initRes)));
+				Console.writeln(Unicode::Widen(err));
 			}
 
 			return true;
